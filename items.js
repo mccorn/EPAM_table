49f68fcbb973cc72bcfcb2c8c6163d536e68daaa
mccorn <mc.corn@mail.ru>
2018-01-24 16:10:29 +0300

    Add files via upload

---
+++ items.js
@@ -0,0 +1,24 @@
+     
+	var item0 = {
+                name: "Item Name",
+                count: 0,
+                price: 999.000,
+        };
+        var  item1 = {
+                name: "Ivanov R",
+                count: 100,
+                price: .00,
+        };
+         var  item2 = {
+                name: "Petrov A",
+                count: 10,
+                price: 12.03,
+        };
+         var  item3 = {
+                name: "Sidorov K",
+                count: 1,
+                price: 12.409,
+        };
+
+        var items = [item1,item2,item0,item3];
+	
